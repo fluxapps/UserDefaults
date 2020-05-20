d470b292cd552edb0c6299e649c3e86fd1a026d3
root <knjackson@upei.ca>
2020-05-20 09:47:50 -0400

    Update to 5.4

--- plugin.php
+++ plugin.php
@@ -6,5 +6,6 @@ $id = 'usrdef';
 $version = '1.7.7';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '6.999';
+
 $responsible = 'studer + raimann ag - Team Custom 1';
 $responsible_mail = 'support-custom1@studer-raimann.ch';
